d1a5999781a9bbe03d82c2640a635fb1a20fb6b3
Aayman <aayman.khalid1@gmail.com>
2020-02-28 13:57:26 +0500

    Edits

--- new.py
+++ new.py
@@ -2,3 +2,4 @@
 print('This line is written in the second file in the repo!')
 print('Some edits made in the second file!')
 print('Changes made directly to the github repository online!')
+print('This change is being made through the laptop!')
