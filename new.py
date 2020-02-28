65f7d3bb387e3c8ae6adbe02b04b2cd646d56825
Aayman-star <58264653+Aayman-star@users.noreply.github.com>
2020-02-28 13:55:56 +0500

    Update new.py

--- new.py
+++ new.py
@@ -1,3 +1,4 @@
 #The scond file in the repository
 print('This line is written in the second file in the repo!')
 print('Some edits made in the second file!')
+print('Changes made directly to the github repository online!')
